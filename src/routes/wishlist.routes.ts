9d9e36ecc115c5786cf4d4fc99c7eb045544f5c0
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-12-17 17:01:07 +0100

    Update wishlist.routes.ts

--- src/routes/wishlist.routes.ts
+++ src/routes/wishlist.routes.ts
@@ -11,52 +11,52 @@ const router = Router();
 
 // ! POST /api/wishlists - crea wishlist
 router.post(
-    '/',
+    "/",
     validate(createWishlistSchema),
     // TODO: controller.createWishlist
 );
 
 // ! GET /api/wishlists/:id - Dettaglio wishlist
 router.get(
-    '/:id',
-    validate(idParamSchema, 'params'),
+    "/:id",
+    validate(idParamSchema, "params"),
     // TODO: controller.getWishlistById
 );
 
 // ! PUT /api/wishlists/:id - Modifica wishlist
 router.put(
-    '/:id',
-    validate(idParamSchema, 'params'),
+    "/:id",
+    validate(idParamSchema, "params"),
     validate(updateWishlistSchema),
     // TODO: controller.updateWishlist
 );
 
 // ! DELETE /api/wishlists/:id - Elimina wishlist
 router.delete(
-    '/:id',
-    validate(idParamSchema, 'params'),
+    "/:id",
+    validate(idParamSchema, "params"),
     // TODO: controller.deleteWishlist
 );
 
 // ! POST /api/wishlists/:id/publish - Pubblica wishlist
 router.post(
-    '/:id/publish',
-    validate(idParamSchema, 'params'),
+    "/:id/publish",
+    validate(idParamSchema, "params"),
     // TODO: controller.publishWishlist
 );
 
 // ! POST /api/wishlists/:id/gifts - Aggiungi regalo
 router.post(
-    '/:id/gifts',
-    validate(idParamSchema, 'params'),
+    "/:id/gifts",
+    validate(idParamSchema, "params"),
     validate(createGiftSchema),
     // TODO: controller.addGiftToWishlist
 );
 
 // ! GET /api/wishlists/:id/gifts - Lista regali
 router.get(
-    '/:id/gifts',
-    validate(idParamSchema, 'params'),
+    "/:id/gifts",
+    validate(idParamSchema, "params"),
     // TODO: controller.getGiftsByWishlistId
 );
 
