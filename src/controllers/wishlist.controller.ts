902f74b5d5b2e767aa2ecdf87371232544229e0d
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-12-19 15:51:46 +0100

    Refactor wishlist controller for improved readability and add remove, publish, and addGift functionalities

--- src/controllers/wishlist.controller.ts
+++ src/controllers/wishlist.controller.ts
@@ -1,8 +1,13 @@
-import { Request, Response, NextFunction } from 'express';
-import { AppError } from '../middleware/errorHandler';
-import * as wishlistService from '../services/wishlist.service';
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../middleware/errorHandler";
+import * as wishlistService from "../services/wishlist.service";
+import { Certificate } from "node:crypto";
 
-export const create = async (req: Request, res: Response, next: NextFunction) => {
+export const create = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
     try {
         const wishlist = await wishlistService.create(req.body);
         res.status(201).json(wishlist);
@@ -12,13 +17,17 @@ export const create = async (req: Request, res: Response, next: NextFunction) =>
     }
 };
 
-export const getById = async (req: Request, res: Response, next: NextFunction) => {
+export const getById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
     try {
         const { id } = req.params;
         const wishlist = await wishlistService.getByIdWithGifts(Number(id));
 
         if (!wishlist) {
-            throw new AppError(404, 'Wishlist non trovata!');
+            throw new AppError(404, "Wishlist non trovata!");
         }
 
         res.json(wishlist);
@@ -28,17 +37,24 @@ export const getById = async (req: Request, res: Response, next: NextFunction) =
     }
 };
 
-export const update = async (req: Request, res: Response, next: NextFunction) => {
+export const update = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
     try {
         const { id } = req.params;
         const wishlist = await wishlistService.getById(Number(id));
 
         if (!wishlist) {
-            throw new AppError(404, 'Wishlist non trovata!');
+            throw new AppError(404, "Wishlist non trovata!");
         }
 
         if (wishlist.is_published) {
-            throw new AppError(403, 'Impossibile modificare una wishlist pubblicata!');
+            throw new AppError(
+                403,
+                "Impossibile modificare una wishlist pubblicata!",
+            );
         }
 
         const updated = await wishlistService.update(Number(id), req.body);
@@ -47,4 +63,76 @@ export const update = async (req: Request, res: Response, next: NextFunction) =>
         console.error(error);
         next(error);
     }
-}
+};
+
+export const remove = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const { id } = req.params;
+        const deleted = await wishlistService.remove(Number(id));
+
+        if (!deleted) {
+            throw new AppError(404, "Wishlist non trovata!");
+        }
+
+        res.json({ message: "Wishlist eliminata con successo!" });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+};
+
+export const publish = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const { id } = req.params;
+        const wishlist = await wishlistService.publish(Number(id));
+
+        if (!wishlist) {
+            throw new AppError(404, "Wishlist non trovata!");
+        }
+
+        if (wishlist.is_published) {
+            throw new AppError(403, "Wishlist già pubblicata");
+        }
+
+        // Verifica che ci siano i regali
+        // TODO: Bisogna aggiungere i gifts services prima
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+};
+
+export const addGift = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const { id } = req.params;
+        const wishlist = await wishlistService.getById(Number(id));
+
+        if (!wishlist) {
+            throw new AppError(404, "Wishlist non trovata");
+        }
+
+        if (wishlist.is_published) {
+            throw new AppError(
+                403,
+                "Impossibile aggiungere regali a una wishlist pubblicata",
+            );
+        }
+
+        // TODO: Bisogna aggiungere i gifts services prima
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+};
